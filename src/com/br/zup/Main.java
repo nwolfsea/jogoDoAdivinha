7d2b54263ff587ea92949aa4f0785c73d34f3e52
natalia.palota <natalia.palota@zup.com.br>
2021-02-02 16:43:03 -0300

    Nova entrega mínima

--- src/com/br/zup/Main.java
+++ src/com/br/zup/Main.java
@@ -1,29 +1,51 @@
 package com.br.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    public static Scanner entrada() { return new Scanner(System.in); }
+    public static Scanner createScanner() {
+        return new Scanner(System.in);
+    }
+
+    public static Scanner entrada() {
+        return new Scanner(System.in);
+    }
 
     public static void main(String[] args) {
-        int numero1 = 2;
-        int numero2 = 1;
-        int numero3 = 3;
-        int numero4 = 0;
+        int pontos = 0;
+        boolean continuar = true;
+        int contador = 0;
+        List<Integer> erros = new ArrayList<>();
+        List<Integer> acertos = new ArrayList<>();
 
-        System.out.println("Digite o número:");
-        numero1 = entrada().nextInt();
+        while (continuar) {
+            Random numeroAleatorio = new Random();
+            int numero = numeroAleatorio.nextInt(5);
+            System.out.println("Digite um número");
+            int resposta = createScanner().nextInt();
 
-        if (numero1 == 2) {
-            System.out.println("Parabéns, você ganhou 10 pontos.");
-        } else if (numero1 == numero2) {
-            System.out.println("Parabéns, você ganhou 05 pontos.");
-        } else if (numero1 == 3) {
-            System.out.println("Parabéns, você ganhou 05 pontos.");
-        } else if (numero4 == 0) {
-            System.out.println("Você perdeu.");
+            if (resposta == numero) {
+                System.out.println("Você acertou! Ganhou10 pontos!");
+                acertos.add(numero);
+                pontos = pontos + 10;
+                System.out.println("Seu total de pontos é: " + pontos);
+                contador++;
+
+            } else if (resposta != numero) {
+                if (numero - resposta == 1) {
+                    System.out.println("O valor digitado é " + resposta + " e o número sorteado é: " + numero
+                            + " Você ganhou 5 pontos");
+                    pontos = pontos + 5;
+                    erros.add(numero);
+                } else if (numero - resposta == -1) {
+                    System.out.println("Você perdeu o jogo!");
+
+                }
+            }
         }
-
     }
 }
